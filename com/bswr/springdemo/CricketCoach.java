a9ea0f368b70a4c6adaf0d0ebb97976acedfee7a
Leyton <hanlei9876@gmail.com>
2020-08-21 12:50:26 +1000

    inject values into bean

--- com/bswr/springdemo/CricketCoach.java
+++ com/bswr/springdemo/CricketCoach.java
@@ -4,6 +4,36 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	
+	private String emailAddress;
+	private String team;
+	
+	
+	// getter for emailAdress
+	public String getEmailAddress() {
+		System.out.println("CricketCoach: inside getter method - getEmailAddress");
+		return emailAddress;
+	}
+	
+	// setter for emailAdress
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+	
+	// getter for team
+	public String getTeam() {
+		System.out.println("CricketCoach: inside getter method - getTeam");
+		return team;
+	}
+	
+	// setter for team
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
+	
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
